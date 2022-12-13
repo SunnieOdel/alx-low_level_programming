05970e6a4beb4e093ce45c7d930547de2625f507
SunnieOdel <Sunnie_odel@yahoo.com>
2022-12-12 16:43:32 -0800

    My forst line of C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * man - print a line of code using puts
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
