ba10a68977d7044bc7645343d73fbddba5d4499c
SunnieOdel <Sunnie_odel@yahoo.com>
2022-12-12 18:38:18 -0800

    Task six

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print out sizes of data types in C
+ * code by sunnie
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
+printf("Size of a int: %lu byte(S)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
+return (0);
+}
