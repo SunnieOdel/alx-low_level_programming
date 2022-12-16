a15030f20c4e96bf0aa1b527df0927d3f5054eb7
SunnieOdel <Sunnie_odel@yahoo.com>
2022-12-15 18:14:04 -0800

    Task 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ * owned by sunnie
+ *
+*/
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+
+}
