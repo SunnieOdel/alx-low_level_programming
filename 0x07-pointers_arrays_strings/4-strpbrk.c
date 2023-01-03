a86b29804b99b7d49a66cbceb6d461a1698ecc2d
SunnieOdel <Sunnie_odel@yahoo.com>
2023-01-03 15:29:34 -0800

    4 strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,8 +2,10 @@
 
 /**
  * _strpbrk - function that searches a string for any of a set of bytes
+ * 
  * @s:first occurrence in the string
  * @accept: matches one of the bytes, or @NULL if no such byte
+ * 
  * Return: a pointer to the byte
 */
 
@@ -20,5 +22,6 @@ char *_strpbrk(char *s, char *accept)
 		}
 		s++;
 	}
+	
 	return (NULL);
 }
