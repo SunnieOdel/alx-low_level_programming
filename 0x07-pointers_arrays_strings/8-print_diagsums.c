c17a83a1d076ffec77767d8d6fca9369d4c84d3e
SunnieOdel <Sunnie_odel@yahoo.com>
2023-01-03 17:15:23 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,13 +9,13 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, l = 0, r = 0
+	int i, j, p, l = 0, r = 0;
 
-		for (i = 0; i < size; i++)
-		{
-			p = (i * size) + i;
-			l += *(a + p);
-		}
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
 
 	for (j = 0; j < size; j++)
 	{
